4a731e5416b4424adebb56209d521b9aa4fb38af
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-10-05 13:52:04 +1100

    Added support to add subscription to alarms sns topic

--- alarm_sns.tf
+++ alarm_sns.tf
@@ -1,3 +1,10 @@
 resource "aws_sns_topic" "alarms_topic" {
   name = "${var.service_name}-${var.env}-Alarms"
 }
+
+resource "aws_sns_topic_subscription" "subscription" {
+  for_each   = { for idx, sub in local.sns_subscriptions : idx => sub }
+  topic_arn  = aws_sns_topic.alarms_topic.arn
+  protocol   = each.value.protocol
+  endpoint   = each.value.endpoint
+}
