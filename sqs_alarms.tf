74c7bac5377108f5a5ff2f1046fbc606ce3cca74
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-10-16 14:12:02 +1100

    Update migrated branch with latest changes

--- sqs_alarms.tf
+++ sqs_alarms.tf
@@ -39,3 +39,23 @@ resource "aws_cloudwatch_metric_alarm" "sqs_approx_age_of_oldest_message_alarm" 
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "lambda_dlq_messages_sent_alarm" {
+  for_each = { for idx, lambda_obj in local.lambda_list : idx => lambda_obj }
+
+  alarm_name          = "${each.value.lambda}-DLQMessagesSentAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "NumberOfMessagesSent"
+  namespace           = "AWS/SQS"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+
+  alarm_description = "Alarm triggered when messages are sent to the DLQ of ${each.value.lambda}"
+  alarm_actions     = [local.sns_topic_arn]
+
+  dimensions = {
+    QueueName = "${each.value.lambda}-dlq"
+  }
+}
+
