fe6a3c139376f4e0cd25c163b006885da99ae60b
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-10-17 11:22:21 +1100

    fix sqs alarms

--- sqs_alarms.tf
+++ sqs_alarms.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_metric_alarm" "sqs_approx_num_messages_visible_alarm" {
   for_each = { for idx, queue in local.sqs_list : idx => queue }
 
-  alarm_name          = format("%s-sqs_approx_num_messages_visible_alarm", each.value.queue)
+  alarm_name          = format("%s-sqs_approx_num_messages_visible_alarm", each.value.name)
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "ApproximateNumberOfMessagesVisible"
@@ -21,7 +21,7 @@ resource "aws_cloudwatch_metric_alarm" "sqs_approx_num_messages_visible_alarm" {
 resource "aws_cloudwatch_metric_alarm" "sqs_approx_age_of_oldest_message_alarm" {
   for_each = { for idx, queue in local.sqs_list : idx => queue }
 
-  alarm_name          = format("%s-sqs_approx_age_of_oldest_message_alarm", each.value.queue)
+  alarm_name          = format("%s-sqs_approx_age_of_oldest_message_alarm", each.value.name)
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "ApproximateAgeOfOldestMessage"
