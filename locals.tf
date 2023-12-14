cf95dde021c5833db32aa358bffb4ffbb361ee5d
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-12-14 12:12:39 +1100

    Parameterize api alarm thresholds

--- locals.tf
+++ locals.tf
@@ -9,14 +9,21 @@ locals {
 }
 
 locals {
-  default_thresholds = {
+  default_api_thresholds = {
     error_4xx_threshold            = 5
     error_5xx_threshold            = 2
     latency_threshold              = 1500
     integration_latency_threshold  = 2500
   }
-  merged_apis = [for api in var.resource_list.apis : merge(local.default_thresholds, api)]
+  merged_apis = [for api in var.resource_list.apis : merge(local.default_api_thresholds, { 
+    api                            = api.api,
+    error_4xx_threshold            = api.error_4xx_threshold != null ? api.error_4xx_threshold : local.default_api_thresholds.error_4xx_threshold,
+    error_5xx_threshold            = api.error_5xx_threshold != null ? api.error_5xx_threshold : local.default_api_thresholds.error_5xx_threshold,
+    latency_threshold              = api.latency_threshold != null ? api.latency_threshold : local.default_api_thresholds.latency_threshold,
+    integration_latency_threshold  = api.integration_latency_threshold != null ? api.integration_latency_threshold : local.default_api_thresholds.integration_latency_threshold
+  })]
 }
+
 
 locals {
   sns_topic_arn = aws_sns_topic.alarms_topic.arn
