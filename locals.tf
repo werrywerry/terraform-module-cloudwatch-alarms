a13e5b72d723726de61948f05c5440c0b0f952ae
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-12-15 12:54:24 +1100

    Enable combining overrides and defaults

--- locals.tf
+++ locals.tf
@@ -15,13 +15,14 @@ locals {
     latency_threshold              = 1500
     integration_latency_threshold  = 2500
   }
-  merged_apis = [for api in var.resource_list.apis : merge(
-    {
-      api = api.api
-    }, 
-    local.default_api_thresholds,
-    contains(keys(var.api_thresholds), api.api) ? var.api_thresholds[api.api] : {}
-  )]
+
+  merged_apis = [for api in var.resource_list.apis : {
+    api = api.api
+    error_4xx_threshold            = contains(keys(var.api_thresholds[api.api]), "error_4xx_threshold") && var.api_thresholds[api.api]["error_4xx_threshold"] != null ? var.api_thresholds[api.api]["error_4xx_threshold"] : local.default_api_thresholds.error_4xx_threshold
+    error_5xx_threshold            = contains(keys(var.api_thresholds[api.api]), "error_5xx_threshold") && var.api_thresholds[api.api]["error_5xx_threshold"] != null ? var.api_thresholds[api.api]["error_5xx_threshold"] : local.default_api_thresholds.error_5xx_threshold
+    latency_threshold              = contains(keys(var.api_thresholds[api.api]), "latency_threshold") && var.api_thresholds[api.api]["latency_threshold"] != null ? var.api_thresholds[api.api]["latency_threshold"] : local.default_api_thresholds.latency_threshold
+    integration_latency_threshold  = contains(keys(var.api_thresholds[api.api]), "integration_latency_threshold") && var.api_thresholds[api.api]["integration_latency_threshold"] != null ? var.api_thresholds[api.api]["integration_latency_threshold"] : local.default_api_thresholds.integration_latency_threshold
+  }]
 }
 
 locals {
@@ -34,8 +35,8 @@ locals {
     dynamo          = dynamo.dynamo
     read_units      = dynamo.read_units
     write_units     = dynamo.write_units
-    read_capacity_threshold  = lookup(var.dynamo_thresholds, dynamo.dynamo, local.default_dynamo_thresholds[dynamo.dynamo]).read_capacity_threshold
-    write_capacity_threshold = lookup(var.dynamo_thresholds, dynamo.dynamo, local.default_dynamo_thresholds[dynamo.dynamo]).write_capacity_threshold
+    read_capacity_threshold  = contains(keys(var.dynamo_thresholds[dynamo.dynamo]), "read_capacity_threshold") && var.dynamo_thresholds[dynamo.dynamo]["read_capacity_threshold"] != null ? var.dynamo_thresholds[dynamo.dynamo]["read_capacity_threshold"] : local.default_dynamo_thresholds[dynamo.dynamo].read_capacity_threshold
+    write_capacity_threshold = contains(keys(var.dynamo_thresholds[dynamo.dynamo]), "write_capacity_threshold") && var.dynamo_thresholds[dynamo.dynamo]["write_capacity_threshold"] != null ? var.dynamo_thresholds[dynamo.dynamo]["write_capacity_threshold"] : local.default_dynamo_thresholds[dynamo.dynamo].write_capacity_threshold
   }]
 }
 
@@ -47,7 +48,7 @@ locals {
   merged_eventbridges = [for eventbridge in var.resource_list.eventbridges : {
     name          = eventbridge.name
     ruleName      = eventbridge.ruleName
-    eventbridge_dead_letter_threshold  = lookup(var.eventbridge_thresholds, eventbridge.name, local.default_eventbridge_thresholds[eventbridge.name]).eventbridge_dead_letter_threshold
+    eventbridge_dead_letter_threshold  = contains(keys(var.eventbridge_thresholds[eventbridge.name]), "eventbridge_dead_letter_threshold") && var.eventbridge_thresholds[eventbridge.name]["eventbridge_dead_letter_threshold"] != null ? var.eventbridge_thresholds[eventbridge.name]["eventbridge_dead_letter_threshold"] : local.default_eventbridge_thresholds[eventbridge.name].eventbridge_dead_letter_threshold
   }]
 }
 
@@ -67,13 +68,13 @@ locals {
     concurrency     = lambda.concurrency
     memory          = lambda.memory
     timeout         = lambda.timeout
-    success_rate_threshold      = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).success_rate_threshold
-    errors_threshold     = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).errors_threshold
-    duration_threshold     = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).duration_threshold
-    memory_underutilization_threshold  = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).memory_underutilization_threshold
-    memory_overutilization_threshold = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).memory_overutilization_threshold
-    concurrent_executions_threshold     = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).concurrent_executions_threshold
-    throttles_threshold     = lookup(var.lambda_thresholds, lambda.lambda, local.default_lambda_thresholds[lambda.lambda]).throttles_threshold
+    success_rate_threshold      = contains(keys(var.lambda_thresholds[lambda.lambda]), "success_rate_threshold") && var.lambda_thresholds[lambda.lambda]["success_rate_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["success_rate_threshold"] : local.default_lambda_thresholds[lambda.lambda].success_rate_threshold
+    errors_threshold            = contains(keys(var.lambda_thresholds[lambda.lambda]), "errors_threshold") && var.lambda_thresholds[lambda.lambda]["errors_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["errors_threshold"] : local.default_lambda_thresholds[lambda.lambda].errors_threshold
+    duration_threshold          = contains(keys(var.lambda_thresholds[lambda.lambda]), "duration_threshold") && var.lambda_thresholds[lambda.lambda]["duration_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["duration_threshold"] : local.default_lambda_thresholds[lambda.lambda].duration_threshold
+    memory_underutilization_threshold = contains(keys(var.lambda_thresholds[lambda.lambda]), "memory_underutilization_threshold") && var.lambda_thresholds[lambda.lambda]["memory_underutilization_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["memory_underutilization_threshold"] : local.default_lambda_thresholds[lambda.lambda].memory_underutilization_threshold
+    memory_overutilization_threshold  = contains(keys(var.lambda_thresholds[lambda.lambda]), "memory_overutilization_threshold") && var.lambda_thresholds[lambda.lambda]["memory_overutilization_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["memory_overutilization_threshold"] : local.default_lambda_thresholds[lambda.lambda].memory_overutilization_threshold
+    concurrent_executions_threshold   = contains(keys(var.lambda_thresholds[lambda.lambda]), "concurrent_executions_threshold") && var.lambda_thresholds[lambda.lambda]["concurrent_executions_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["concurrent_executions_threshold"] : local.default_lambda_thresholds[lambda.lambda].concurrent_executions_threshold
+    throttles_threshold               = contains(keys(var.lambda_thresholds[lambda.lambda]), "throttles_threshold") && var.lambda_thresholds[lambda.lambda]["throttles_threshold"] != null ? var.lambda_thresholds[lambda.lambda]["throttles_threshold"] : local.default_lambda_thresholds[lambda.lambda].throttles_threshold
   }]
 }
 
@@ -88,16 +89,16 @@ locals {
   }}
 
   merged_rds = [for rds in var.resource_list.rdss : {
-    rds          = rds.rds
-    total-iops      = rds.total-iops
-    total-memory     = rds.total-memory
-    total-storage     = rds.total-storage
-    cpu_utilization_threshold  = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).cpu_utilization_threshold
-    total_iops_threshold = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).total_iops_threshold
-    read_latency_threshold  = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).read_latency_threshold
-    write_latency_threshold = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).write_latency_threshold
-    freeable_memory_threshold  = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).freeable_memory_threshold
-    free_storage_space_threshold = lookup(var.rds_thresholds, rds.rds, local.default_rds_thresholds[rds.rds]).free_storage_space_threshold
+    rds                      = rds.rds
+    total-iops               = rds.total-iops
+    total-memory             = rds.total-memory
+    total-storage            = rds.total-storage
+    cpu_utilization_threshold        = contains(keys(var.rds_thresholds[rds.rds]), "cpu_utilization_threshold") && var.rds_thresholds[rds.rds]["cpu_utilization_threshold"] != null ? var.rds_thresholds[rds.rds]["cpu_utilization_threshold"] : local.default_rds_thresholds[rds.rds].cpu_utilization_threshold
+    total_iops_threshold             = contains(keys(var.rds_thresholds[rds.rds]), "total_iops_threshold") && var.rds_thresholds[rds.rds]["total_iops_threshold"] != null ? var.rds_thresholds[rds.rds]["total_iops_threshold"] : local.default_rds_thresholds[rds.rds].total_iops_threshold
+    read_latency_threshold           = contains(keys(var.rds_thresholds[rds.rds]), "read_latency_threshold") && var.rds_thresholds[rds.rds]["read_latency_threshold"] != null ? var.rds_thresholds[rds.rds]["read_latency_threshold"] : local.default_rds_thresholds[rds.rds].read_latency_threshold
+    write_latency_threshold          = contains(keys(var.rds_thresholds[rds.rds]), "write_latency_threshold") && var.rds_thresholds[rds.rds]["write_latency_threshold"] != null ? var.rds_thresholds[rds.rds]["write_latency_threshold"] : local.default_rds_thresholds[rds.rds].write_latency_threshold
+    freeable_memory_threshold        = contains(keys(var.rds_thresholds[rds.rds]), "freeable_memory_threshold") && var.rds_thresholds[rds.rds]["freeable_memory_threshold"] != null ? var.rds_thresholds[rds.rds]["freeable_memory_threshold"] : local.default_rds_thresholds[rds.rds].freeable_memory_threshold
+    free_storage_space_threshold     = contains(keys(var.rds_thresholds[rds.rds]), "free_storage_space_threshold") && var.rds_thresholds[rds.rds]["free_storage_space_threshold"] != null ? var.rds_thresholds[rds.rds]["free_storage_space_threshold"] : local.default_rds_thresholds[rds.rds].free_storage_space_threshold
   }]
 }
 
@@ -109,8 +110,8 @@ locals {
 
   merged_sqs = [for sqs in var.resource_list.queues : {
     name          = sqs.name
-    sqs_approx_num_messages_visible_threshold  = lookup(var.sqs_thresholds, sqs.name, local.default_sqs_thresholds[sqs.name]).sqs_approx_num_messages_visible_threshold
-    sqs_approx_age_of_oldest_message_threshold  = lookup(var.sqs_thresholds, sqs.name, local.default_sqs_thresholds[sqs.name]).sqs_approx_age_of_oldest_message_threshold
+    sqs_approx_num_messages_visible_threshold  = contains(keys(var.sqs_thresholds[sqs.name]), "sqs_approx_num_messages_visible_threshold") && var.sqs_thresholds[sqs.name]["sqs_approx_num_messages_visible_threshold"] != null ? var.sqs_thresholds[sqs.name]["sqs_approx_num_messages_visible_threshold"] : local.default_sqs_thresholds[sqs.name].sqs_approx_num_messages_visible_threshold
+    sqs_approx_age_of_oldest_message_threshold  = contains(keys(var.sqs_thresholds[sqs.name]), "sqs_approx_age_of_oldest_message_threshold") && var.sqs_thresholds[sqs.name]["sqs_approx_age_of_oldest_message_threshold"] != null ? var.sqs_thresholds[sqs.name]["sqs_approx_age_of_oldest_message_threshold"] : local.default_sqs_thresholds[sqs.name].sqs_approx_age_of_oldest_message_threshold
   }]
 }
 
