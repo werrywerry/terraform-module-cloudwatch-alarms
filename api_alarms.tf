123d04822eba3e3d818611e6eb9999de93711271
Joel Bradley <joel.bradley3@det.nsw.edu.au>
2023-12-20 10:55:03 +1100

    Fix wrong alarm statistics

--- api_alarms.tf
+++ api_alarms.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_metric_alarm" "api_4xx_errors_alarm" {
   metric_name         = "4XXError"
   namespace           = "AWS/ApiGateway"
   period              = "300"
-  statistic           = "SampleCount"
+  statistic           = "Sum"
   threshold           = each.value.error_4xx_threshold
   alarm_description   = "This metric checks for 4xx errors in the ${each.value.api} API"
   alarm_actions       = [local.sns_topic_arn]
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_metric_alarm" "api_5xx_errors_alarm" {
   metric_name         = "5XXError"
   namespace           = "AWS/ApiGateway"
   period              = "300"
-  statistic           = "SampleCount"
+  statistic           = "Sum"
   threshold           = each.value.error_5xx_threshold
   alarm_description   = "This metric checks for 5xx errors in the ${each.value.api} API"
   alarm_actions       = [local.sns_topic_arn]
